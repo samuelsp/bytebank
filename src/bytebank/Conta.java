ef911af830f86d0718ff73ead320dc6ed2e66a1e
samuelsp <samp_81@yahoo.com.br>
2022-12-03 09:42:30 -0300

    Classes principais ByteBank

---
+++ src/bytebank/Conta.java
@@ -0,0 +1,75 @@
+package bytebank;
+
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
+	
+	public Conta(int agencia, int numero){
+		this.agencia = agencia;
+		this.numero = numero;
+		Conta.total++;		
+	}
+	
+	public void deposita(double valor){
+		if (valor > 0) {
+			this.saldo += valor;
+		}
+	}
+	
+	public boolean saca(double valor){
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		}
+		
+		return false;
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (this.numero > 0) {
+			this.numero = numero;
+		}
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+	
+}
